b1f860aafe30399e6e48df38a07719a28bfdbded
Adamethazine <147160974+Adamethazine@users.noreply.github.com>
2023-11-09 12:39:32 +0100

    piji

---
+++ webAccelResponsiv.js
@@ -0,0 +1,67 @@
+
+// bemærk hvis man skal oprette flere canvas og køre ikke-global mode
+// https://github.com/processing/p5.js/wiki/p5.js-overview#instantiation--namespace
+// https://www.youtube.com/watch?v=Su792jEauZg
+// derman kan man oprette instans som objekt, hvori sketchen kører
+// Og man kan oprette flere canvas med samme sketch eller
+// forskellige sketches i forskellige canvas
+// instans mode skal closure på variable - namespacing - vs global mode med globale variable
+
+// tilføj knapper, og tilføj eventlistener til knappen, funktionskald
+// 10 spring til siden hurtigt nok - gør baggrunden helt hvid...
+
+let y = 10;
+let r = 200;
+let g = 180;
+let b = 0;
+let ned = true;
+let rystet = 0;
+let flyttet = 0;
+let timer = 0; // Timer variabel
+
+function setup() {
+  canvas = createCanvas(300, 550, 'beholder');
+  textSize(12);
+  canvas.elt.style.border = '5px solid black';
+  canvas.elt.style.boxSizing = 'border-box';
+  canvas.elt.style.borderRadius = '20px';
+  canvas.parent('#beholder');
+  canvas.elt.style.width = '100%';
+  canvas.elt.style.height = '100%';
+
+  const parentDiv = select('#beholder').elt;
+  const p = select('#test1').elt;
+  parentDiv.insertBefore(canvas.elt, p);
+}
+
+function draw() {
+  background(255, 0, 0);
+  strokeWeight(10);
+
+  if (accelerationX > 70) {
+    r = random(0, 256);
+    g = random(0, 256);
+    b = random(0, 256);
+    if (rystet % 2 == 0) {
+      ned = !ned;
+      timer++; // Øg timeren hver gang rystet er lige
+    }
+    rystet++;
+    if(rystet > 30)
+    rystet = 0
+  }
+  
+  // Vis timeren i midten af skærmen
+  fill(255);
+  text('Antal Englehop: ' + timer, width / 4 - 40, height / 2);
+
+  push();
+  textSize(15);
+  text('Lav 30 Englehop' , 30, 100);
+  pop();
+  
+}
+
+function deviceMoved() {
+  flyttet++;
+}
